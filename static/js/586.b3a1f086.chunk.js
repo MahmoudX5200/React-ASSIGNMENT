16ec06d2ea89050a5ffaa1f086c0c41f2f181575
unknown <mahmoudmomo63@gmail.com>
2023-10-06 02:08:09 +0200

    Updates

---
+++ static/js/586.b3a1f086.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinalproject=self.webpackChunkfinalproject||[]).push([[586],{586:function(e,n,r){r.r(n),r.d(n,{default:function(){return t}});r(2791);var c=r(184);function t(){return(0,c.jsx)("h1",{className:"my-5",children:"Profile"})}}}]);
+//# sourceMappingURL=586.b3a1f086.chunk.js.map
